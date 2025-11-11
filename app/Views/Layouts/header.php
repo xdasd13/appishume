6d7c4f257a74161c0419947d33d5fc2385f22a4d
Morgan Bondioli <morganmurilobondioli@gmail.com>
2025-11-11 09:31:04 -0500

    cambios v.3

--- app/Views/Layouts/header.php
+++ app/Views/Layouts/header.php
@@ -434,28 +434,6 @@
 
       <div class="container">
         <div class="page-inner">
-          <div class="page-header">
-            <h4 class="page-title">Dashboard</h4>
-            <ul class="breadcrumbs">
-              <li class="nav-home">
-                <a href="#">
-                  <i class="icon-home"></i>
-                </a>
-              </li>
-              <li class="separator">
-                <i class="icon-arrow-right"></i>
-              </li>
-              <li class="nav-item">
-                <a href="#">Pages</a>
-              </li>
-              <li class="separator">
-                <i class="icon-arrow-right"></i>
-              </li>
-              <li class="nav-item">
-                <a href="#">Starter Page</a>
-              </li>
-            </ul>
-          </div>
           <div class="page-category">
             <!-- Antes de cerrar el body -->
 
