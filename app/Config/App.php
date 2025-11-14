cf3ff130e5d42891755d09f86351fd5f085a45a2
Leonel Saravia <saraviaalss28@gmail.com>
2025-11-14 10:28:32 -0500

    ROUTES ACTUALIZADOS

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://appishume.test/';
+    public string $baseURL = 'http://appishume1.test/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
